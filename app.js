3f3365474167c857994f9f2a08163a6f9f664909
Mohammad Al Amin <alamin.cse.99@gmail.com>
2025-08-26 08:37:29 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const mongoose = require('mongoose');
+const router = require('./src/routes/api')
+require('dotenv').config()
+
+
+// databse cennection
+let URL = process.env.mongodbUrl;
+let option = { user: process.env.user, pass: process.env.user, autoIndex: true }
+mongoose.connect(URL, option).then(() => {
+    console.log('Database connected...');
+}).catch((err) => {
+    console.log(err)
+})
+
+
+//  seccurity middleware implemantation
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// Routing Implement
+app.use("/api/v1", router);
+app.use("*", (req, res) => {
+    res.status(404).json({ status: "fail", data: "Not Found" })
+})
+
+module.exports = app;
