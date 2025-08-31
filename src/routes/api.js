28d36933e5cd8749e756be40d1cc49a288e13103
Mohammad Al Amin <alamin.cse.99@gmail.com>
2025-08-31 22:40:34 +0600

    collection CRUD operation done

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 const userController = require("../controllers/userController");
+const collectionController = require("../controllers/collectionController");
 const authorize = require("../middlewares/authorize");
 
 // product routes
@@ -15,6 +16,12 @@ router.post("/auth/register", userController.userRegistration);
 router.post("/auth/login", userController.userLogin);
 router.get("/auth/user/profile", authorize(["admin", "customer"]), userController.userProfile);
 router.post("/auth/user/profile", authorize(["admin", "customer"]), userController.updateUserProfile);
+
+// collection routes
+router.post("/collection", authorize(["admin"]), collectionController.createCollection);
+router.get("/collection", collectionController.getCollections);
+router.patch("/collection/:id", authorize(["admin"]), collectionController.updateCollection);
+router.delete("/collection/:id", authorize(["admin"]), collectionController.deleteCollection);
 
 
 module.exports = router;
