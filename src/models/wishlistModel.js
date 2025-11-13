004742be1db39195d6239f14fc6dec90f76c5f34
MohammadAlamin99 <alamin.cse.99@gmail.com>
2025-11-14 04:44:45 +0600

    refector wish create

--- src/models/wishlistModel.js
+++ src/models/wishlistModel.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const wishlistSchema = new Schema({
-    userId: { type: mongoose.Types.ObjectId, ref: "users", required: true },
-    productId: { type: mongoose.Types.ObjectId, ref: "products" },
+    userId: { type: mongoose.Types.ObjectId, ref: "users", required: true, unique: true },
+    productId: [{ type: mongoose.Types.ObjectId, ref: "products" }],
 }, { timestamps: true, versionKey: false, });
 
 const wishlistModel = mongoose.model("wishlists", wishlistSchema);
